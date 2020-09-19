7baad54e6b4aa963a6a39d6f942a362f6cf54f6e
dev.Ton <ktb.kittipob@gmail.com>
2020-09-19 14:43:29 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskItemComponent } from './task-item/task-item.component';
+import { TaskAddComponent } from './task-add/task-add.component';
+import { TaskCheckboxComponent } from './task-checkbox/task-checkbox.component';
+
+import { TaskService } from '../app/task.service';
+import { TabButtonComponent } from './ui/tab-button/tab-button.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskItemComponent,
+    TaskAddComponent,
+    TaskCheckboxComponent,
+    TabButtonComponent,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TaskService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
